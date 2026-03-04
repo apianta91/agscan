67227e55f254d2a5eb2f127c16e2176038263254
apianta91 <a.pianta91@gmail.com>
2026-03-04 15:59:30 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'ag-scan-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './ag-scan-512.png',
+  './ag-scan-180.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
